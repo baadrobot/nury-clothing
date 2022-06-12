e2902673ed5be2a2d4335af7d96535e79468bda1
unknown <baadrobot92@gmail.com>
2022-06-12 13:14:28 +0200

    added in our addToCart feature

--- src/components/card-dropdown/card-dropdown.component.jsx
+++ src/components/card-dropdown/card-dropdown.component.jsx
@@ -1,11 +1,20 @@
 import './card-dropdown.styles.scss';
+import CartItem from '../cart-item/cart-item.component';
 import Button from '../button/button.component';
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
+
 
 const CardDropdown = () => {
+    const {cartItems} = useContext(CartContext);
 
     return (
         <div className='cart-dropdown-container'>
-            <div className='cart-items'/>
+            <div className='cart-items'>
+                {cartItems.map((item) => 
+                    <CartItem key={item.id} cartItem={item} />
+                )}
+            </div>
             <Button>GO TO CHECKOUT</Button>
         </div>
     )
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,14 +1,51 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+
+// cart adding function that is also decides if it should add new items to cart
+// or if there already that kind of item just add a quantity of it
+const addCartItem = (cartItems, productToAdd) => {
+    // find if carItems contains productToAdd
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === productToAdd.id
+    )
+
+    // if above is found then increment the quantity and return the function
+    if(existingCartItem) {
+        return cartItems.map((cartItem) => 
+            cartItem.id === productToAdd.id
+            ? {...cartItem, quantity: cartItem.quantity + 1}
+            : cartItem
+        )
+    }
+
+    // if none of above matched then it means the function valled 1st time
+    //  then return new array with modified cartItems/ new cart item and add quantity 1
+    return [...cartItems, {...productToAdd, quantity: 1}];
+}
 
 export const CartContext = createContext({
     isCartOpen: false,
-    // указываем что это ф-ия, но не уверен что это делать обязательно
     setIsCartOpen: () => {},
+    cartItems: [],
+    addItemToCart: () => {},
+    cartCount: 0,
 })
 
 export const СartProvider = ({children}) => {
-    const [isCartOpen, setIsCartOpen] = useState('hidden');
-    const value = {isCartOpen, setIsCartOpen};
+    const [isCartOpen, setIsCartOpen] = useState(false);
+    const [cartItems, setCartItems] = useState([]);
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd));
+    }
+
+    // cart counter
+    const [cartCount, setCartCount] = useState(0);
+    useEffect(() => {
+        const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
+        setCartCount(newCartCount);
+    }, [cartItems])
+
+    const value = {isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount};
 
     return (
         <CartContext.Provider value={value}>{children}</CartContext.Provider>
