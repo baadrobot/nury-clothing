bcc5093641e93f17f4da6ebea00a97553945b5e9
baadrobot <baadrobot92@gmail.com>
2022-07-26 21:32:59 +0200

    added Stripe payment module with Netlify hosting

--- src/components/routes/checkout/checkout.component.jsx
+++ src/components/routes/checkout/checkout.component.jsx
@@ -4,6 +4,7 @@ import {useSelector} from 'react-redux';
 import { selectCartItems, selectCartTotal } from '../../../store/cart/cart.selector.js';
 
 import CheckoutItem from '../../checkout-item/checkout-item.component';
+import PaymentForm from '../../payment-form/payment-form.component.jsx';
 
 const Checkout = () => {
     const cartItems = useSelector(selectCartItems);
@@ -32,6 +33,7 @@ const Checkout = () => {
                 <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
             ))}
             <Total>Total: ${cartTotal}</Total>
+            <PaymentForm/>
         </CheckoutContainer>
     )
 }
